e13c862fd987b29c72ee4e1a115c74f2c12be207
Leksa <admin@petiteweb.ru>
2025-04-26 13:12:34 +0300

    init template

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.scss"
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+	<>
+		Hello React!
+	</>
+)
